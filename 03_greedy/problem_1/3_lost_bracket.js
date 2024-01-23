c87c859ba2177670849b416757d4362b4ea44b0a
ChanLee_KR <cl700120@gmail.com>
2024-01-23 13:58:36 +0900

    modify convention

--- 03_greedy/problem_1/3_lost_bracket.js
+++ 03_greedy/problem_1/3_lost_bracket.js
@@ -7,6 +7,6 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('-');
 const input = `10+20+30+40`.trim().split('-');
 const result = input.reduce((acc, cur, idx) => {
     const sum = cur.split('+').map(Number).reduce((a, b) => a + b);
-    return idx == 0 ? acc += sum : acc -= sum;
+    return idx == 0 ? acc + sum : acc - sum;
 }, 0);
 console.log(result);
