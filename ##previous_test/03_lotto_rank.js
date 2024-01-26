f580251be2b71214f52cc1e34963a96db40da839
ChanLee_KR <cl700120@gmail.com>
2024-01-26 20:56:50 +0900

    rename file & add solved problem

--- ##previous_test/03_lotto_rank.js
+++ ##previous_test/03_lotto_rank.js
@@ -1,4 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/77484 (로또의 최고 순위와 최저 순위)
+// 2021 Dev-Matching: 웹 백엔드 개발자(상반기)
 
 function solution(lottos, win_nums) {
     const solve = lottos.reduce(([ rank, cnt ], num) => {
---
+++ ##previous_test/04_report_result.js
@@ -0,0 +1,64 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/92334 (신고 결과 받기)
+// 2022 KAKAO BLIND RECRUITMENT
+
+/*
+// 처음 풀이, 코드 너무 더러워서 뒤에 다시 씀
+function solution(id_list, report, k) {
+
+    const organizedRecords = report
+        .map((record) => record.split(' '))
+        .reduce((obj, [ complainant, defendant ]) => {
+            obj[complainant] = obj[complainant] ? ([ ...obj[complainant], defendant ]) : ([ defendant ]);
+            return obj;
+        }, ({}));
+
+    const dedupedRecords = Object.keys(organizedRecords)
+        .reduce((obj, user) => (obj[user] = [ ...new Set(organizedRecords[user]) ], obj), ({}));
+
+    const usersReportCount = Object.keys(dedupedRecords)
+        .reduce((obj, userKey) => {
+            dedupedRecords[userKey].forEach((user) => obj[user] = obj[user] ? obj[user] + 1 : 1);
+            return obj;
+        }, ({}));
+
+    const bannedUsers = Object.keys(usersReportCount)
+        .reduce((arr, user) => {
+            return usersReportCount[user] >= k ? [ ...arr, user ] : arr;
+        }, ([]))
+    
+    const totalReports = id_list
+        .reduce((obj, user) => {
+            if (!dedupedRecords[user]) return (obj[user] = 0, obj);
+            const count = dedupedRecords[user].reduce((cnt, bannedUser) => {
+                return bannedUsers.filter(target => target == bannedUser).length ? cnt + 1 : cnt;
+            }, 0);
+            return (obj[user] = count, obj);
+        }, ({}));
+    
+    const result = Object.keys(totalReports).map((key) => totalReports[key]);
+
+    return result;
+}
+*/
+
+function solution(id_list, report, k) {
+
+    const organizedRecords = [ ...new Set(report) ]
+        .map((record) => record.split(' '))
+    
+    const bannedUsers = organizedRecords
+        .reduce((map, [ , target ]) => {
+            return (map.set(target, map.get(target) + 1 || 1), map);
+        }, new Map());
+    
+    const usersReportCount = organizedRecords
+        .reduce((map, [ user, reportedUser ]) => {
+            return bannedUsers.get(reportedUser) >= k ? 
+                (map.set(user, map.get(user) + 1 || 1), map) : map;
+        }, new Map());
+
+    return id_list.map(user => usersReportCount.get(user) ?? 0);
+}
+
+console.log(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2)); // [ 2, 1, 1, 0 ]
+console.log(solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3)); // [ 0, 0 ]
