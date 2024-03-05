c0cf2312ce586b2d03d6618dfd618cee6566011c
ChanLee_KR <cl700120@gmail.com>
2024-03-05 20:45:23 +0900

    add solved problem

--- 04_binary_search/parametric_search/4_management_pocket_money.js
+++ 04_binary_search/parametric_search/4_management_pocket_money.js
@@ -15,4 +15,30 @@ const input = `7 5
 
 const [ n, m, ...pocketMoneys ] = input;
 
-console.log(pocketMoneys);
+let start = Math.max(...pocketMoneys);
+let end = pocketMoneys.reduce((a, b) => a + b, 0);
+
+let unit = Infinity;
+while (start <= end) {
+    const mid = parseInt((start + end) / 2);
+    let count = 1;
+    let sum = 0;
+    for (const money of pocketMoneys) {
+        if (money > mid) {
+            count = Infinity;
+            break;
+        }
+        if (sum + money <= mid) sum += money;
+        else {
+            count += 1;
+            sum = money;
+        }
+    }
+    if (count <= m) {
+        end = mid - 1;
+        unit = Math.min(unit, mid);
+    }
+    else start = mid + 1;
+ }
+
+console.log(unit);
