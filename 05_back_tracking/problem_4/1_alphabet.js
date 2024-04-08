431f1ec011d91022069eb60d57d7aa3ce07e3ffe
ChanLee_KR <cl700120@gmail.com>
2024-04-08 13:00:06 +0900

    add solved problem

--- 05_back_tracking/problem_4/1_alphabet.js
+++ 05_back_tracking/problem_4/1_alphabet.js
@@ -13,32 +13,29 @@ HFGCF
 HMCHH`.trim().split('\n');
 
 const [ r, c ] = input[0].split(' ').map(Number);
-const alphabets = [];
-for (let i = 1; i <= r; i += 1) alphabets.push(input[i]);
+const alphabets = new Array(r);
+for (let i = 1; i <= r; i += 1) alphabets[i - 1] = input[i].trim().split('');
 
 const dx = [ -1, 1,  0, 0 ];
 const dy = [  0, 0, -1, 1 ];
-// const visited = new Array(26).fill(false);
-const visited = new Set(); // 방문한 적 있는 알파벳 집합
+const visited = new Array(26).fill(false);
 
 let maxDepth = 0; // 최대 깊이
 function dfs(depth, x, y) {
     maxDepth = Math.max(maxDepth, depth);
     for (let i = 0; i < 4; i += 1) {
-        const [ nx, ny ] = [ x + dx[i], y + dy[i] ];
+        const nx = x + dx[i];
+        const ny = y + dy[i];
         if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-        // if (visited[alphabets[nx][ny].charCodeAt() - 65]) continue;
-        if (visited.has(alphabets[nx][ny])) continue;
-        // visited[alphabets[nx][ny].charCodeAt() - 65] = true;
-        visited.add(alphabets[nx][ny]);
-        dfs(depth + 1, nx, ny);
-        // visited[alphabets[nx][ny].charCodeAt() - 65] = false;
-        visited.delete(alphabets[nx][ny]);
+        if (!visited[alphabets[nx][ny].charCodeAt() - 65]) {
+            visited[alphabets[nx][ny].charCodeAt() - 65] = true;
+            dfs(depth + 1, nx, ny);
+            visited[alphabets[nx][ny].charCodeAt() - 65] = false;
+        }
     }
 }
 
-// visited[alphabets[0][0]] = true;
-visited.add(alphabets[0][0]);
+visited[alphabets[0][0].charCodeAt() - 65] = true;
 dfs(1, 0, 0);
 
 console.log(maxDepth);
